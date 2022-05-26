263e898462a3bccc359e5c9c663b720d7d725d4a
shoaibahmadsakib <shoaibahmadsakib@gmail.com>
2022-05-26 14:43:46 +0600

    add user admin

--- index.js
+++ index.js
@@ -176,7 +176,12 @@ async function run() {
     //make admin
   
    
-
+    app.get("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await UserEmail.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
 
     app.put("/user/admin/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
